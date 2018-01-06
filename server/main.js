110ed01a55e8403f3f8820958239838f69108fb1
Ayush <ayush.singhvi@nyu.edu>
2018-01-06 13:36:13 -0500

    delete unnessary files

--- server/main.js
+++ server/main.js
@@ -13,7 +13,3 @@ app.listen(port, function () {
 
 module.exports = app;
 
-https.get('https://api.github.com/users/mosane/repos', function (err, res){
-	if(err) console.error("this is an error", err)
-	console.log('this is the res', res)
-})
